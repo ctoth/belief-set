a2df03e8fdd7adc7b4d792898d5a836c5d3f1f77
Christopher Toth <q.alpha@gmail.com>
2026-05-01 17:40:58 -0600

    test: assert public package exports

--- tests/test_package_import.py
+++ tests/test_package_import.py
@@ -4,4 +4,24 @@ from __future__ import annotations
 def test_package_imports() -> None:
     import belief_set
 
-    assert belief_set.__all__ == []
+    assert set(belief_set.__all__) == {
+        "BOTTOM",
+        "TOP",
+        "Atom",
+        "BeliefSet",
+        "Formula",
+        "RevisionOutcome",
+        "RevisionTrace",
+        "SpohnEpistemicState",
+        "World",
+        "conjunction",
+        "disjunction",
+        "equivalent",
+        "expand",
+        "full_meet_contract",
+        "lexicographic_revise",
+        "negate",
+        "restrained_revise",
+        "revise",
+        "theory_subset",
+    }
